7a1b79d86d5b81d939d30345ec95f15319d9a434
Wuradzu <savinykhvladislav@gmail.com>
2023-01-07 12:02:12 +0200

    ADD: router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Detail from "./components/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="detail/:id" element={<Detail/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
